705b280323fe2d30a40da34bacaff4c11db7027b
shrutimani <sman0054@student.monash.edu>
2022-10-13 00:54:12 +1100

    final

--- vega_lite_vs.js
+++ vega_lite_vs.js
@@ -1,4 +1,4 @@
-var vg_1 = "dropout_rates.vg.json";
+var vg_1 = "new_drop_out.vg.json";
 vegaEmbed("#vg1", vg_1).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
