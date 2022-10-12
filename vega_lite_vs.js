4ce2f21c586b179fd921b46f3bef5314e621ab39
shrutimani <sman0054@student.monash.edu>
2022-10-12 23:32:07 +1100

    new ones

--- vega_lite_vs.js
+++ vega_lite_vs.js
@@ -1,4 +1,4 @@
-var vg_1 = "visualization.vg.json";
+var vg_1 = "visualization_dropout_rates.vg.json";
 vegaEmbed("#vg1", vg_1).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
@@ -6,6 +6,18 @@ vegaEmbed("#vg1", vg_1).then(function(result) {
 
 var vg_2 = "int_visualization.vg.json";
 vegaEmbed("#vg2", vg_2).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "visualization_over_years.vg.json";
+vegaEmbed("#vg3", vg_3).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "visualization_india.vg.json";
+vegaEmbed("#vg4", vg_4).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
