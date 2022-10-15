fd933a360769fddec8329f3f35204ceb10e9c56e
shrutimani <sman0054@student.monash.edu>
2022-10-15 20:07:46 +1100

    new new

--- vega_lite_vs.js
+++ vega_lite_vs.js
@@ -1,43 +1,48 @@
+var opt = {
+    actions: false
+}
 var vg_1 = "new_drop_out.vg.json";
-vegaEmbed("#vg1", vg_1).then(function(result) {
+vegaEmbed("#vg1", vg_1, opt).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "int_visualization.vg.json";
-vegaEmbed("#vg2", vg_2).then(function(result) {
+vegaEmbed("#vg2", vg_2, opt).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "visualization_over_years.vg.json";
-vegaEmbed("#vg3", vg_3).then(function(result) {
+vegaEmbed("#vg3", vg_3, opt).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_4 = "visualization_india.vg.json";
-vegaEmbed("#vg4", vg_4).then(function(result) {
+vegaEmbed("#vg4", vg_4, opt).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_5 = "visualization_zoom_population.vg.json";
-vegaEmbed("#vg5", vg_5).then(function(result) {
+vegaEmbed("#vg5", vg_5, opt).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 
 var vg_6 = "visualization_gender.vg.json";
-vegaEmbed("#vg6", vg_6).then(function(result) {
+vegaEmbed("#vg6", vg_6, opt).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 
 var vg_7 = "visualization_categories.vg.json";
-vegaEmbed("#vg7", vg_7).then(function(result) {
+vegaEmbed("#vg7", vg_7, opt).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+
