424f7b01df3ec019d8a53d396cf6c8a43a72893f
shrutimani <sman0054@student.monash.edu>
2022-10-15 20:16:31 +1100

    delete some vis

--- vega_lite_vs.js
+++ vega_lite_vs.js
@@ -1,46 +1,37 @@
 var opt = {
     actions: false
 }
-var vg_1 = "new_drop_out.vg.json";
-vegaEmbed("#vg1", vg_1, opt).then(function(result) {
+var dropout_rates = "new_drop_out.vg.json";
+vegaEmbed("#dropout_rates", dropout_rates, opt).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "int_visualization.vg.json";
-vegaEmbed("#vg2", vg_2, opt).then(function(result) {
-// Access the Vega view instance
-//(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-
-var vg_3 = "visualization_over_years.vg.json";
-vegaEmbed("#vg3", vg_3, opt).then(function(result) {
-// Access the Vega view instance
-//(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-
-var vg_4 = "visualization_india.vg.json";
-vegaEmbed("#vg4", vg_4, opt).then(function(result) {
-// Access the Vega view instance
-//(https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-
-var vg_5 = "visualization_zoom_population.vg.json";
-vegaEmbed("#vg5", vg_5, opt).then(function(result) {
+var scatter = "int_visualization.vg.json";
+vegaEmbed("#scatter", scatter, opt).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 
-var vg_6 = "visualization_gender.vg.json";
-vegaEmbed("#vg6", vg_6, opt).then(function(result) {
+
+
+var population_zoom = "visualization_zoom_population.vg.json";
+vegaEmbed("#population_zoom", population_zoom, opt).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 
-var vg_7 = "visualization_categories.vg.json";
-vegaEmbed("#vg7", vg_7, opt).then(function(result) {
+var gender = "visualization_gender.vg.json";
+vegaEmbed("#gender", gender, opt).then(function(result) {
+// Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+var categories = "visualization_categories.vg.json";
+vegaEmbed("#categories", categories, opt).then(function(result) {
 // Access the Vega view instance
 //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
